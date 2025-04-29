ab9f8cfb6361e4c50429c3d1cc807664aa2fabaa
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-04-29 07:55:32 +0700

    2.9 membuat database

---
+++ app/src/main/java/com/example/tanya_ustadz/database/DoaDao.kt
@@ -0,0 +1,27 @@
+package com.example.tanya_ustadz.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.tanya_ustadz.model.Doa
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DoaDao {
+
+    @Insert
+    suspend fun insert(doa: Doa)
+
+    @Update
+    suspend fun update(doa: Doa)
+
+    @Query("SELECT * FROM doa ORDER BY tanggal DESC")
+    fun getDoa(): Flow<List<Doa>>
+
+    @Query("SELECT * FROM doa WHERE id = :id")
+    suspend fun getDoaById(id : Long):Doa?
+
+    @Query("DELETE FROM doa WHERE id = :id")
+    suspend fun deleteById(id : Long)
+}
---
+++ app/src/main/java/com/example/tanya_ustadz/database/DoaDb.kt
@@ -0,0 +1,34 @@
+package com.example.tanya_ustadz.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.tanya_ustadz.model.Doa
+
+@Database(entities = [Doa::class], version = 1, exportSchema = false)
+abstract class DoaDb : RoomDatabase() {
+
+    abstract val dao:DoaDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: DoaDb? = null
+
+        fun getInstance(context: Context): DoaDb{
+            synchronized(this){
+                var instance = INSTANCE
+
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        DoaDb::class.java,
+                        "doa.db"
+                    ).build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
